cce0086eccb936e4ef513204cf713981862a0068
HoffOskar <oskar.hoff@gmail.com>
2025-02-14 09:36:51 +0100

    SALI added

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# QSAR BRD4
+# Background
+## Drug Discovery - Oncology
+- Overall cost to bring a new anticancer drug to the market ranges from **$944 million** to **$4.54 billion** (2019 USD) - M. Schlander et *al.*: *PharmacoEconomics* 39, 1243–1269 (**2021**)
+- Virtual screening can make the drug discovery funnel more efficient:
+
+![drug discovery funnel](images/Funnel.png)
+
+(Source: https://multispaninc.com/drug-discovery-services-and-high-throughput-screening)
+
+## The Target - BRD4
+Bromodomain-containing protein 4 (BRD4) is a significant target due to its role in regulating gene expression and its involvement in cancer and other diseases.
+
+**1. Role and Function:**
+
+BRD4 is a member of the bromodomain and extra-terminal (BET) family of proteins, which play a crucial role in regulating gene expression.
+It specifically recognizes and binds acetylated lysines on histones, a process critical for the regulation of transcriptional machinery.
+By interacting with acetylated histones, BRD4 can recruit other transcriptional coactivators and chromatin-modifying enzymes, thereby influencing gene expression patterns.
+
+**2. Biological Significance:**
+
+**Cancer:** BRD4 has been implicated in several cancers, including leukemia, lymphoma, and solid tumors. It contributes to the regulation of oncogenes such as MYC and is involved in the formation of super-enhancer regions, which drive the expression of oncogenes.
+**Inflammatory Diseases:** Beyond cancer, BRD4 is also associated with inflammatory conditions and has been explored as a target for treating diseases like rheumatoid arthritis.
+
+**3. Drug Discovery:**
+
+**Inhibitors:** Targeting BRD4 with small molecules, known as BET inhibitors, has shown promise in preclinical studies and clinical trials. These inhibitors can disrupt BRD4's interaction with acetylated lysines, thereby modulating gene expression.
+**Degradation:** BRD4 is a common target in the field of targeted-protein degradation (molecular glues and PROTACs). Which are outside the scope of this project. 
+
+**References:**
+
+- Filippakopoulos, P., et al. (2012) "Selective inhibition of BET bromodomains." Nature, 468(7327), 1067-1073. DOI: 10.1038/nature09504
+- Zhang, J., et al. (2017) "Structural basis for the inhibition of the BET bromodomain by small molecules." Journal of Medicinal Chemistry, 60(12), 5600-5611. DOI: 10.1021/acs.jmedchem.6b01630
+- Wu, L., et al. (2019) "Development of potent and selective inhibitors targeting the BRD4 bromodomain." Nature Communications, 10, 2511. DOI: 10.1038/s41467-019-10544-0
+
+
+
+# Usage
+
+## Functions
+**render_mol()**
+
+In this repository chemical structures are predominantly displayed as RDKit mol objects in pandas DataFrames. The function `rdkit.Chem.PandasTools.ChangeMoleculeRendering(df)` is renamed `render_mol()`. It renders the structures if the jupyter notebooks cell output setting are **text/html**:
+
+![html](images/render_html.png)
+
+Output settings as **text/plain** shows the mol objects id:
+
+![plain](images/render_plain.png)
--- utils/pd_mol.py
+++ utils/pd_mol.py
@@ -1,36 +1,37 @@
 ### Imports
-from rdkit import Chem
-
-from rdkit.Chem import Draw
-from rdkit.Chem import PandasTools
-from rdkit.Chem import rdCoordGen
-
-from rdkit.Chem.MolStandardize import rdMolStandardize
-
-from rdkit.Chem.Draw import rdMolDraw2D
-from rdkit.Chem.Draw import rdDepictor
-from rdkit.Chem.Draw import IPythonConsole
-
+import copy
 import math
 
-import copy
+from rdkit import Chem
+from rdkit.Chem import Draw, PandasTools, rdCoordGen
+from rdkit.Chem.Draw import rdDepictor
+from rdkit.Chem.MolStandardize import rdMolStandardize
+
+
+def render_mol(df):
+    """
+    Renders the "Mol" column of a DataFrame for visual inspection of chemical structures.
+    """
+    PandasTools.ChangeMoleculeRendering(df)
+    return df
 
 
 def to_clipboard_smiles(df):
-    '''Loads a DataFrame with a "Mol" column to_clipboard'''
+    """Loads a DataFrame with a "Mol" column to_clipboard"""
     # Create a copy of the input DataFrame to avoid changes to the original object
     df_copy = copy.deepcopy(df)
 
     # Generate SMILES for each molecule in the 'Mol' column
-    df_copy['Smiles'] = df_copy['Mol'].apply(lambda mol: Chem.MolToSmiles(mol))
+    df_copy["Smiles"] = df_copy["Mol"].apply(lambda mol: Chem.MolToSmiles(mol))
 
     # Drop the 'Mol' column
-    df_copy.drop(columns=['Mol'], inplace=True)
+    df_copy.drop(columns=["Mol"], inplace=True)
 
     # Copy the modified DataFrame to clipboard
     df_copy.to_clipboard(index=True)
 
     return
+
 
 def convert_ic50nM_to_pic50(IC50_value):
     pIC50_value = 9 - math.log10(IC50_value)
@@ -39,74 +40,81 @@ def convert_ic50nM_to_pic50(IC50_value):
 
 ### Visualize the mol columns set as Grid
 def grid_view(mol_col):
-    '''
+    """
     Accepts a pandas.Series of Mol objects ("Mol" column) and displays all structures in a grid
-    '''
-    return Draw.MolsToGridImage(mol_col,
-                     molsPerRow=5,              # Adjust this number to control how many molecules appear per row
-                     subImgSize=(400, 400),     # Optional: adjust the size of each sub-image
-                     useSVG=True                # Optional: use SVG format for better quality in Jupyter notebooks
-                    )
+    """
+    return Draw.MolsToGridImage(
+        mol_col,
+        molsPerRow=5,  # Adjust this number to control how many molecules appear per row
+        subImgSize=(400, 400),  # Optional: adjust the size of each sub-image
+        useSVG=True,  # Optional: use SVG format for better quality in Jupyter notebooks
+    )
+
 
 ### Functions
-def standardize_mol_col(mol_col, 
-                          largest_frag=True, 
-                          remove_charge=True, 
-                          tautomerize=False, 
-                          normalize=True,
-                          remove_stereo=False,
-                          cal_2D_coord_default=True,
-                          cal_2D_coord_deep=False,
-                          cleanup=True
-                          ):
-    '''
+def standardize_mol_col(
+    mol_col,
+    largest_frag=True,
+    remove_charge=True,
+    tautomerize=False,
+    normalize=True,
+    remove_stereo=False,
+    cal_2D_coord_default=True,
+    cal_2D_coord_deep=False,
+    cleanup=True,
+):
+    """
     Standardize a pandas.Series of Mol objects ("Mol" column).
-    '''
-    
+    """
+
     # largest fragment only
-    print(f'largest_frag={largest_frag} (rdMolStandardize.FragmentParent):')
+    print(f"largest_frag={largest_frag} (rdMolStandardize.FragmentParent):")
     if largest_frag:
-        mol_col = mol_col.progress_apply(rdMolStandardize.FragmentParent)          
-    
-    # remove charges 
-    print(f'remove_charge={remove_charge} (rdMolStandardize.Uncharger().uncharge):')
+        mol_col = mol_col.progress_apply(rdMolStandardize.FragmentParent)
+
+    # remove charges
+    print(f"remove_charge={remove_charge} (rdMolStandardize.Uncharger().uncharge):")
     if remove_charge:
-        mol_col = mol_col.progress_apply(rdMolStandardize.Uncharger().uncharge)    
-    
+        mol_col = mol_col.progress_apply(rdMolStandardize.Uncharger().uncharge)
+
     # generate most populated tautomer # slow only use when necessary
-    print(f'tautomerize={tautomerize} (rdMolStandardize.CanonicalTautomer)')
+    print(f"tautomerize={tautomerize} (rdMolStandardize.CanonicalTautomer)")
     if tautomerize:
-        mol_col = mol_col.progress_apply(rdMolStandardize.CanonicalTautomer)       
-    
+        mol_col = mol_col.progress_apply(rdMolStandardize.CanonicalTautomer)
+
     # normalize (not sure if necessary)
-    print(f'normalize={normalize} (rdMolStandardize.Normalize):') 
+    print(f"normalize={normalize} (rdMolStandardize.Normalize):")
     if normalize:
-        mol_col = mol_col.progress_apply(rdMolStandardize.Normalize)               
-    
+        mol_col = mol_col.progress_apply(rdMolStandardize.Normalize)
+
     # remove stereoinformation
-    print(f'remove_stereo={remove_stereo} (rdMolStandardize.Normalize):')
+    print(f"remove_stereo={remove_stereo} (rdMolStandardize.Normalize):")
     if remove_stereo:
-        mol_col = mol_col.progress_apply(rdMolStandardize.StereoParent)            
-    
+        mol_col = mol_col.progress_apply(rdMolStandardize.StereoParent)
+
     ### 2D-coordinates are only calculated once
     if cal_2D_coord_deep:
-        print(f'cal_2D_coord_default=not required (rdDepictor.Compute2DCoords):')
-        print(f'cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):')
+        print("cal_2D_coord_default=not required (rdDepictor.Compute2DCoords):")
+        print(f"cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):")
         # alternative to fix 2D-coordinates # straightens alkynes to 180°
         mol_col.progress_apply(rdCoordGen.AddCoords)
-       
+
     elif cal_2D_coord_default:
-        print(f'cal_2D_coord_default={cal_2D_coord_default} (rdDepictor.Compute2DCoords):')
+        print(
+            f"cal_2D_coord_default={cal_2D_coord_default} (rdDepictor.Compute2DCoords):"
+        )
         # default to fix 2D-coordinates # important for combinatorial libraries
         mol_col.progress_apply(rdDepictor.Compute2DCoords)
-        print(f'cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):')
+        print(f"cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):")
     else:
-        print(f'cal_2D_coord_default={cal_2D_coord_default} (rdDepictor.Compute2DCoords):')
-        print(f'cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):')                   
-    
+        print(
+            f"cal_2D_coord_default={cal_2D_coord_default} (rdDepictor.Compute2DCoords):"
+        )
+        print(f"cal_2D_coord_deep={cal_2D_coord_deep} (rdCoordGen.AddCoords):")
+
     # clean up visualization
-    print(f'cleanup={cleanup} (rdMolStandardize.Cleanup):')
+    print(f"cleanup={cleanup} (rdMolStandardize.Cleanup):")
     if cleanup:
-        mol_col = mol_col.progress_apply(rdMolStandardize.Cleanup) 
-        
+        mol_col = mol_col.progress_apply(rdMolStandardize.Cleanup)
+
     return mol_col
